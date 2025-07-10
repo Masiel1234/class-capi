e95b80ee659ed9cd9f5f932d9bb45cbe83576bda
Sammy Carolina Arias Riveros <samcaro2233@gmail.com>
2025-07-10 11:26:53 -0500

    updated profile

---
+++ src/pages/blog/profile/index.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { deleteActiveUser, getActiveUser, type userData } from "@/lib/LocalStorage";
+import { useRouter } from "next/navigation"; 
+
+const Profile = () => {
+    const [activeUser, setActiveUser] = useState<userData>();
+    const router = useRouter();
+
+    useEffect(() => {
+        const data = getActiveUser();
+        if (data == null) {
+            router.push("/login");
+        } else {
+            setActiveUser(data);
+        }
+    }, []);
+
+    const handleLogOut = () => {
+        deleteActiveUser();
+        router.push("/login");
+    };
+
+    return (
+        <>
+            <div className="flex items-center gap-4">
+                <Image
+                    src={activeUser?.avatarUrl ?? '@/../public/images/pfp.jpg'}
+                    alt={activeUser?.username ?? 'usuario'}
+                    className="w-10 h-10 rounded-full object-cover"/>
+                <p className="text-sm font-medium capitalize">
+                    {"welcome"} {activeUser?.username}!
+                </p>
+                <button
+                    onClick={handleLogOut}
+                    className="bg-transparent border-none text-sm font-bold cursor-pointer px-3 py-2 transition-all duration-300 ease-in-out uppercase hover:text-red-600">
+                    {"logout"}
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default Profile;
