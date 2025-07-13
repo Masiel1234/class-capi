b1be6ea9764b1461a45cdf82964c890e8db74a09
Sammy Carolina Arias Riveros <samcaro2233@gmail.com>
2025-07-13 09:54:52 -0500

    estructure corrected

--- src/pages/blog/playlist/index.tsx
+++ src/pages/blog/playlist/index.tsx
@@ -1,41 +1,25 @@
 'use client'
 import Button from '@/common/components/Button/SubmitButton'
-import '@/pages/blog/category/pop';
 export default function Playlist() {
   return (
-    <div>
-      <h1>Saved Songs</h1>
-      <div className='boards-container'>
-        <div className='board'>
-          <p>Board 1</p>
-          <div className='btn-add-contain'>
-            <Button variant="add" text="add song" to="/blog" onClick={() => { }} />
-          </div>
+    <div className='form-wrapper'>
+      <h1 className='form-title'>Add New Song</h1>
+      <form action="" className='form-container'>
 
-          <img src="https://i.pinimg.com/736x/68/8b/4c/688b4c9c7527fb8925a840c0cde72360.jpg" alt="img1" />
-          <div className=''>Song Title</div>
-          <div className=''>Song Artist</div>
-        </div>
-        <div className='board'>
-          <p>Board 2</p>
-          <div className='btn-add-contain'>
-            <Button variant="add" text="add song" to="/blog" onClick={() => { }} />
-          </div>
+        <label htmlFor="title" className='form-label'>Song Title:</label>
+        <input type="text" name="title" id="" className='form-input'/><br />
 
-          <img src="https://i.pinimg.com/736x/ec/ce/60/ecce606803c0157558d32344d6cca9df.jpg" alt="img2" />
-          <div className=''>Song Title</div>
-          <div className=''>Song Artist</div>
-        </div>
-        <div className='board'>
-          <p>Board 3</p>
-          <div className='btn-add-contain'>
-            <Button variant="add" text="add song" to="/blog" onClick={() => { }} />
-          </div>
-          <img src="https://i.pinimg.com/736x/9b/8b/12/9b8b12ebe5b3c6aa6efcac27f6a9c78d.jpg" alt="img3" />
-          <div className=''>Song Title</div>
-          <div className=''>Song Artist</div>
-        </div>
-      </div>
+        <label htmlFor="artist" className='form-label'>Artist</label>
+        <input type="text" name="artist" id="" className='form-input'/><br />
+
+        <label htmlFor="album" className='form-label'>Album</label>
+        <input type="text" name="album" id="" className='form-input'/><br />
+
+        <label htmlFor="year" className='form-label'>Year</label>
+        <input type="number" name="year" id="" className='form-input'/><br />
+
+        <Button variant="add" text="add song" to="/" onClick={() => { }} />
+      </form>
     </div>
   )
 }
